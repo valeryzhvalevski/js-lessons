052e858db4aaa26d136c51354af5e87bc621dca0
valeryzhvalevski <105601886+valeryzhvalevski@users.noreply.github.com>
2024-01-28 20:48:33 +0300

    add tsk 3

--- script.js
+++ script.js
@@ -1,144 +1,61 @@
-// // Задание 1
-// // Задайте в коде переменную n с числовым значением.
-// // С помощью логического И в условии проверьте, находится ли переменная n в диапазоне чисел от 0 до 100 включительно.
+// Задание 1
+// Описать в html тег input и тег ul.
+// По нажатию на клавишу в инпуте в список ul должен добавляться элемент li.
+// Содержимое li - нажатая клавиша.
 
-const n = 300;
-const result = (n >= 0 && n <= 100) ? "Переменная n находится в диапазоне от 0 до 100 включительно." :
-"Переменная n не находится в диапазоне от 0 до 100 включительно.";
+// const input = document.getElementById('textInput');
+// const ul = document.getElementById('list');
 
-console.log(result);
+//   input.addEventListener('keydown', function(event) {
+//     if (event.key === 'Enter') {
+//       const li = document.createElement('li');
+//       li.textContent = input.value;
+//       ul.appendChild(li);
+//       input.value = '';
+//     }
+//   });
 
-// // Задание 2
-// // Дан объект с именами и заработными платами:
-// // const engineers = {
-// // Den: 1000,
-// // Matt: 5000,
-// // Steve: 2000
-// // }
-// // Вывести в консоль через шаблонные строки заработную плату всех работников в таком формате:
-// // Заработная плата ххх составляет ххх рублей.
-// // ВНИМАНИЕ. Ваша программа должна работать для любого количества имён и зарплат!
+// Задание 2
+// Вставить в html тег input (просто предусмотреть в разметке).
+// Обрабатывая событие keyup на теге input, выводить в консоль введенный текст каждый раз,
+// как только клиент вписывает любой символ в поле (или стирает любой символ из поля).
+// Вам понадобится считывать значение поля, это input.value
 
-const engineers = {
-Den: 1000,
-Matt: 5000,
-Steve: 2000
-}
-for (let key in engineers) {
-   console.log(`Заработная плата ${[key]} составляет ${engineers[key]} рублей.`);
-}
+// const input = document.getElementById('textInput');
+// input.addEventListener('keyup', () => {
+//     console.log(input.value)
+// })
 
-// // Задание 3
-// // Создать массив из 5 элементов.
-// // Используя цикл for, вывести каждый второй элемент массива в консоль.
+// Задание 3
+// Создать в html форму с инпутом и кнопкой. Также добавить в html тег ul.
+// Когда форма отправляется, добавлять в список тег li. Его содержимое - введенный текст (input.value).
+// После отправки формы инпут должен быть очищен (проставить пустую строку в value).
 
-const arr = [2, 4, 'r', 9, 'l'];
-for (let i = 1; i < arr.length; i += 2) {
-  console.log(arr[i]);
-}
+// const input = document.getElementById("textInput");
+// const ul = document.getElementById("list");
+// const btn = document.getElementById("btn");
 
-// // Задание 4
-// // Есть массив произвольных чисел:
-// // let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
-// // Вывести в консоль значения всех элементов массива и соответствующие им индексы в таком виде:
-// // Индексу 0 соответствует число 42
-// // Индексу 1 соответствует число 65
-// // и т.д.
+// btn.addEventListener("click", function () {
+//   const li = document.createElement("li");
+//   li.textContent = input.value;
+//   ul.appendChild(li);
+//   input.value = "";
+// });
 
-let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70];
-for (let i = 0; i < numbers.length; i++) {
-    console.log(`Индексу ${i} соответствует число ${numbers[i]}`);
-}
+// Задание 4
+// Калькулятор. 
+// Создать в html форму с текстовым input, тегом select, вторым текстовым input и кнопкой. Добавить в html div. 
+// Внутри select будут options - арифметические знаки. В оба инпута пользователь вводит число. 
+// Когда пользователь отправляет форму (событие submit), над двумя числами выполняется действие, 
+// выбранное в select (чтобы получить выбранный пользователем option, мы "забираем" значение  select.value). 
+// Результат отображается в div.
+// 1) решить с помощью if
+// 2) решить с помощью evel (https://developer.mozilla.org/...)
 
 
-// // Задание 5
-// // Дан массив объектов, например:
-// // let questions = [{
-// //     question: "What's the currency of the USA?",
-// //     choices: ["US dollar", "Ruble", "Horses", "Gold"],
-// //     corAnswer: 0
-// // }, {
-// //     question: "Where was the American Declaration of Independence signed?",
-// //     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
-// //     corAnswer: 0
-// // }];
-// // Добавить в каждый объект дополнительное поле usersAnswer со значением null.
-// // Решение должно работать для массива любой длины.
 
-let questions = [
-  {
-    question: "What's the currency of the USA?",
-    choices: ["US dollar", "Ruble", "Horses", "Gold"],
-    corAnswer: 0,
-  },
-  {
-    question: "Where was the American Declaration of Independence signed?",
-    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
-    corAnswer: 0,
-  },
-];
-
-for (let i = 0; i < questions.length; i++) {
-  questions[i].usersAnswer = null;
-}
-console.log(questions);
-
-// // Задание 6
-// // Есть массив произвольных чисел:
-// // let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
-// // 1) Вывести в консоль все элементы массива, используя 2 разных цикла: for-of и for со счетчиком
-// // 2) Посчитать и вывести в консоль сумму элементов в массиве.
-// // Подсказка: в задачах такого типа нужно заранее предусмотреть переменную (например, sum) и записать в нее начальное значение (в случае с суммой - ноль).
-// // Затем уже описать цикл, в котором эта сумма будет считаться (в каждой итерации цикла к ней будет прибавляться следующий элемент массива).
-// // 3) Посчитать и вывести в консоль сумму только четных чисел в массиве.
-// // 4) Найти и вывести в консоль максимальное число массива.
-// // Подсказка: как и в случае с суммой, здесь сначала нужно объявить переменную (например, max), записать в нее первый элемент массива,
-// // а затем уже в цикле искать максимальное значение.
-// // 5) Определить и вывести в консоль индекс максимального числа массива (или индексы, если число встречается более одного раза).
-// // Само максимальное число мы уже нашли в прошлой части задачи, повторно его искать не нужно.
-
-let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70];
-
-for (let element of numbers) {
-   console.log(element);
-}
-
-for (let i = 0; i < numbers.length; i++) {
-   console.log(numbers[i]);
-}
-
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-sum = sum + numbers[i];
-}
-console.log(sum);
-
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 2 === 0) {
-    sum = sum + numbers[i];
-  }
-}
-console.log(sum);
-
-let max = numbers[0];
-for (let element of numbers) {
-  if (element > max) {
-    max = element;
-  }
-}
-console.log(max);
-
-let max = numbers[0]; 
-let indexesOfMax = [0]; 
-
-for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > max) {
-        max = numbers[i];
-        indexesOfMax = [i]; 
-    } else if (numbers[i] === max) {
-        indexesOfMax.push(i); 
-    }
-}
-
-console.log(indexesOfMax);
+// Задание 5
+// Вставить в разметку html тег button без js (просто предусмотреть в разметке). 
+// При наведении на кнопку изменять ее цвет каждый раз рандомным цветом. 
+// При выведении мышки за пределы кнопки поворачивать кнопку на рандомный угол от -180 до 180 градусов. 
+// Использовать обработку событий mouseenter, mouseleave на этой кнопке.
